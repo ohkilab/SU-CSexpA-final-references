67cd7b8766f6ea740a394ddf9cbd4f7d11389e19
Naborisk <github@naborisk.com>
2023-06-09 21:52:41 +0900

    change rg version to index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,80 +1,96 @@
+// 実行する前に../csv/フォルダーにtag.csvとgeotag.csvを用意してください
+//
+// 本コードは以下のコマンドの実装、ripgrepインストール必要がある
+// rg ,test$ ../csv/tag.csv -r '' | rg -f - ../csv/geotag.csv -m 100
+const { spawn } = require('node:child_process')
 const express = require('express')
-const {parse} = require('csv-parse')
-const fs = require('fs')
-
 const app = express()
 const port = 3000
 
-// tag.csvとgeotag.csvのパス
+// tagとgeotagのパス
 const tagPath = '../csv/tag.csv'
 const geotagPath = '../csv/geotag.csv'
 
-app.get('/',(req, res) => {
-  // GETパラメータからtagを取得
-  const key = req.query.tag
-  
-  console.log(`${key} requested`)
+// キャッシュ用変数
+let cache = {}
 
-  const geotagReadStream = fs.createReadStream(geotagPath)
+app.get('/', (req, res) => {
+  // tagを取得して、tag変数に格納する
+  const tag = req.query.tag
 
-  let tags = []
-  let geotags = []
+  // リクエストが来るときに出力する
+  console.log(`requested: ${tag}`)
 
-  const tagParser = fs.createReadStream(tagPath).pipe(parse({delimiter: ',', from_line: 1, skip_records_with_empty_values: true, trim: true}))
+  // キャッシュ変数にすでに結果があるならそのまま返す、そうじゃないとファイルから検索
+  if (cache[tag]) {
+    res.send(cache[tag])
+  } else {
+    // tag.csvとgeotag.csvを検索するプロセスを作成
+    const rgTag = spawn('rg', [`,${tag}$`, tagPath, '-m', '100', '-r', ''])
+    const rgGeotag = spawn('rg', ['-f', '-', geotagPath, , '-m', '100'])
 
-  tagParser
-    .on('data', tagRow => {
-      // デバッグ用のログ、有効時には検索が遅くなる
-      
-      if(tags.length < 100) {
-        if(tagRow[1] === key) {
-          tags.push(tagRow[0])
-          console.log('found' + tags.length)
-        }
-      } else {
-        tagParser.destroy()
+    // 検索結果の変数
+    let searchResult = ''
 
-        const geotagParser = geotagReadStream
-          .pipe(parse({delimiter: ',', from_line: 1, skip_records_with_empty_values: true}))
+    // rgTagプロセスからrgGeotagプロセスにパイプする、パイプ文字(|)と同じ機能
+    rgTag.stdout.pipe(rgGeotag.stdin)
 
-        geotagParser
-          .on('data', row => {
-
-            // console.log(row)
-            if(geotags.length < 100) {
-              if(tags.includes(row[0])) {
-                console.log('found ' + geotags.length + ' geotags')
-                geotags.push(row)
-              }
-            } else {
-              geotagParser.destroy()
-              res.send({
-                tag: key,
-                results: geotags.map(geotag => ({
-                    lat: geotag[2],
-                    lon: geotag[3],
-                    date: geotag[1],
-                    url: geotag[4]
-                  })
-                )
-              })
-            }
-          })
-
-        req.on('close', () => {
-          geotagParser.destroy()
-        })
-      }
+    // tag.csvからデータが来たときのコールバック
+    rgTag.stdout.on('data', data => {
+      // console.log(data.toString())
     })
 
-  // 接続切れたらストリームを破棄する(メモリーリーク防止)
-  req.on('close', () => {
-    // console.log('session closed, destroying streams')
-    tagParser.destroy()
-    geotagReadStream.destroy()
-  })
+    // geotag.csvからデータがきたときのコールバック
+    rgGeotag.stdout.on('data', data => {
+      // console.log(data.toString())
+      searchResult += data
+    })
+
+    // geotag.csvからエラーがきたときのコールバック
+    rgGeotag.stderr.on('data', data => {
+      // console.log(data.toString())
+    })
+
+    // 検索終わったときの処理
+    rgGeotag.on('close', () => {
+      // console.log('closed', searchResult)
+
+      // JSON形式にする処理
+      const searchResultJson = {
+        tag: tag,
+        results: searchResult.trim().split('\n')
+          .map(line => {
+            const geotag = line.split(',')
+
+            return {
+              lat: Number(geotag[2]),
+              lon: Number(geotag[3]),
+              date: geotag[1].replaceAll('"', ''),
+              url: geotag[4]
+            }
+          }).sort((a, b) => new Date(b.date) - new Date(a.date))
+      }
+
+      // クライアントに送信
+      res.send(searchResultJson)
+
+      // キャッシュに格納
+      cache[tag] = searchResultJson
+
+      // 送信・キャッシュしたときの出力
+      console.log(`sent and cached ${tag}`)
+    })
+
+    // 接続が切れたときにプロセスを終了させる
+    req.on('close', () => {
+      console.log('session closed, killing processes')
+      rgTag.kill()
+      rgGeotag.kill()
+    })
+  }
 })
 
+// ポート3000で接続
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
